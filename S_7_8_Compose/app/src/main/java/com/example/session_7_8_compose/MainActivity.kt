8daf7c28cab12e53b8499492e7d915504e59091b
Ian <pristianb007@gmail.com>
2023-10-06 17:57:11 +0700

    Jetpack Compose Exercise

--- S_7_8_Compose/app/src/main/java/com/example/session_7_8_compose/MainActivity.kt
+++ S_7_8_Compose/app/src/main/java/com/example/session_7_8_compose/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.session_7_8_compose
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.session_7_8_compose.ui.theme.Session_7_8_ComposeTheme
 
 class MainActivity : ComponentActivity() {
+    var num:Int =  0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -31,10 +40,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    val num = remember {
+        mutableStateOf(0)
+    }
+
+    Column (
+        verticalArrangement = Arrangement.Center,
         modifier = modifier
-    )
+    ){
+        Text(
+            fontSize = 24.sp,
+            text = "Hello $name",
+            modifier = modifier,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            fontSize = 50.sp,
+            text = num.value.toString(),
+            modifier = modifier,
+            textAlign = TextAlign.Center
+        )
+        Button(onClick = { num.value += 1 }) {
+            Text(text = "Plus 1")
+        }
+    }
 }
 
 
